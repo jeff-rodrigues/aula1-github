86230f71474e2e7f00a2ca726108b012c71f7a67
Jeff Rodrigues <jeffrodrigues07@gmail.com>
2018-09-24 09:59:50 -0300

    Adicionada mensagem de boa noite

--- src/testGithub/first.java
+++ src/testGithub/first.java
@@ -4,6 +4,7 @@ public class first {
 	public static void main(String[] args) {
 		System.out.println("Hello github");
 		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 	}
 
 }
